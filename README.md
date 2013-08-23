aa996433e7f4827207ed362af374181b9f82705d
Michael <michael@fallo.ws>
2013-08-23 21:10:53 +0100

    Version 2.0
    Totally new API for Ajax; now is all in one object and can handle multiple requests at once!

---
+++ README.md
@@ -0,0 +1,11 @@
+## mf.js Javascript Library
+
+A Very Basic Javascript Library.
+
+Please use this link to fetch:
+
+	http://redorkulated.github.io/mf.js/mf.js
+
+	or
+
+		<script type="text/javascript" src="http://redorkulated.github.io/mf.js/mf.js"></script>
--- mf.js
+++ mf.js
@@ -1,90 +1,95 @@
-/* mf.js scripts */
-
+/**
+* mf.js
+* @package com.mf89.js
+* @author Michael Fallows <michael@fallo.ws>
+* @version 2.0
+*/
 /* Basics */
 function $(i){ return document.getElementById(i); }
 function $$(i,t){ return $(i).getElementsByTagName(t); }
 function $x(i,t){ return i.getElementsByTagName(t); }
 
-/* Ajax Version 1.1 */
-function Ajax_getHttpObject(){
-  var xmlhttp;
-  /*@cc_on
-  @if(@_jscript_version >= 5){
-    try{
-      xmlhttp= new ActiveXObject("Msxml2.XMLHTTP");
-    } catch(e) {
-      try{
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-      }catch(E){
-        xmlhttp=false;
-      }
-    }
-  }
-  @else
-  xmlhttp=false;
-  @end @*/
-  if(!xmlhttp && typeof XMLHttpRequest != undefined){
-    try{
-      xmlhttp=new XMLHttpRequest();
-      xmlhttp.overrideMimeType("text/xml");
-    }catch(e){
-      xmlhttp=false;
-    }
-  }
-  return xmlhttp;
-} ServerAjaxTalk= Ajax_getHttpObject();
-function $open(URL,method){ return Ajax_open(URL,method); }
-function Ajax_open(URL,method){
-	if(!method){ var method="GET"; }else{ method= method.toUpperCase(); } if((method != "GET")&&(method != "POST")&&(method != "HEAD")){ method="ERROR"; }
-	var urlEsc= escape(URL);
-	if(urlEsc.match("%3F")){ URL+="&" + Math.random(); }else{ URL+="?" + Math.random(); }
-	ServerAjaxTalk.open(method, URL, true);
-}
-function $send(functionName,content){ return Ajax_send(functionName,content); }
-function Ajax_send(functionName,content){
-	if(!content){ content=null; }
-	if(functionName == "HEAD"){ //Must be head request
-		if(content){ ServerAjaxTalk.onreadystatechange= function(){ if(ServerAjaxTalk.readyState==4){ eval(content +"(Ajax_getHttpStatus());"); } }; }
-		ServerAjaxTalk.send("");
-	}else{
-		ServerAjaxTalk.onreadystatechange= function(){ if((ServerAjaxTalk.readyState==4)&&(ServerAjaxTalk.responseXML)){ eval(functionName +"(ServerAjaxTalk.responseXML);"); } };
-		ServerAjaxTalk.send(content);
-	}
-
-}
-function $stop(){ return Ajax_stop(); }
-function Ajax_stop(){
-	ServerAjaxTalk.abort();
-}
-function $header(header){ return Ajax_getHeader(header); }
-function Ajax_getHeader(header){
-	if(header){
-		return ServerAjaxTalk.getResponseHeader(header);
-	}else{
-		var Headers= escape(ServerAjaxTalk.getAllResponseHeaders());
-		Headers= Headers.split("%0A");
-
-		var list= Array(); var out= Array();
-		var p=0;
-		for(i=0;i<Headers.length;i++){
-			var thisRow= Headers[i].split("%3A%20");
-			thisRow[0]= unescape(thisRow[0]); thisRow[1]= unescape(thisRow[1]);
-			if(thisRow[0] != ""){
-				out[thisRow[0]]= thisRow[1];
-				list[p++]= thisRow[0];
+/* Ajax */
+var $Ajax= {
+	States: {None:0,Open:1,Sent:2,Received:3,ReceivedError:4,Closed:5},
+	_BlankRequest: function(){ return {
+		Core: function (){ var x= new XMLHttpRequest(); x.overrideMimeType("text/xml"); return x; }(),
+		ID: 0, Method: "NONE", URL: "::", Error: false, State: 0
+	}; },
+	_NextRequest: 0, _RequestStack: [],
+	New: function(URL,method){
+		var R= new this._BlankRequest; R.ID= this._NextRequest++;
+		method= (!method?"GET":method.toUpperCase()); if(method != "GET" && method != "POST" && method != "HEAD"){ method="ERROR"; }
+		if(URL.match(/\?/)){ URL+="&" + Math.random(); }else{ URL+="?" + Math.random(); }
+		this._RequestStack[R.ID]= R;
+		this._RequestStack[R.ID].Method= method;
+		this._RequestStack[R.ID].URL= URL;
+		this._RequestStack[R.ID].Core._Ajax= this;
+		this._RequestStack[R.ID].Core._Parent= this._RequestStack[R.ID];
+		this._RequestStack[R.ID].State= this.States.Open;
+		this._RequestStack[R.ID].Core.open(method,URL,true);
+		return R.ID;
+	},
+	Send: function(R,functionCallback,content){
+		if(this._RequestStack[R]){
+			if(!functionCallback){
+				this._RequestStack[R].Error="$Ajax.send callback function not found";
+				console.log("Error with $Ajax Request #"+ R + " : $Ajax.send callback function not found");
+				return false;
+			}
+			if(this._RequestStack[R].Method == "HEAD"){ //Head request, will return no content just headers/status
+				this._RequestStack[R].Core.onreadystatechange= function(){
+					if($Ajax.CoreRequest(R).readyState == 4){
+						this._Parent.State= this._Ajax.States.Received;
+						functionCallback(this.status,this._Parent.ID);
+					}
+				};
+				content="";
+			}else{
+				if(!content){ content= null; }
+				this._RequestStack[R].Core.onreadystatechange= function(){
+					if(this.readyState == 4){
+						this._Parent.State= this._Ajax.States.Received;
+						functionCallback(this.responseXML,this._Parent.ID);
+					}
+				};
+			}
+			this._RequestStack[R].State= this.States.Sent;
+			try{ this._RequestStack[R].Core.send(content); } catch(e){
+				this._RequestStack[R].Error= e;
+				this._RequestStack[R].State= this.States.ReceivedError;
+				console.log("Error with $Ajax Request #"+ R + " : "+ e);
+				functionCallback(false,R);
 			}
 		}
-		return out;
-	}
-}
-function $headers(header){ return Ajax_getHeader(); } 
-function Ajax_getHeaders(){ return Ajax_getHeader(); }
-function $setHeader(header,TxtValue){ return Ajax_setHeader(header,TxtValue); }
-function Ajax_setHeader(header,TxtValue){
-	ServerAjaxTalk.setRequestHeader(header,TxtValue);
-}
-function $status(header){ return Ajax_getHttpStatus(); } 
-function Ajax_getHttpStatus(){
-	return ServerAjaxTalk.status;
-}
-
+	},
+	Header: function (R,H,V){
+		if(!this._RequestStack[R]){ return false; }
+		if(this._RequestStack[R].State != this.States.Received && this._RequestStack[R].State != this.States.Open){ return false; }
+		if(H){
+			if(!V && this._RequestStack[R].State == this.States.Received){ return this._RequestStack[R].Core.getResponseHeader(H); }
+			else if(V && this._RequestStack[R].State == this.States.Open){
+				this._RequestStack[R].Core.setRequestHeader(H,V);
+				return true;
+			}
+			return false;
+		}else if(this._RequestStack[R].State == this.States.Received){
+			var oh= this._RequestStack[R].Core.getAllResponseHeaders().split("\n");
+			var nh= []; var tt;
+			for(i=0;i<oh.length;i++){
+				var t= oh[i].split(": "); if(t[0] != ""){ var tt= t.shift();nh[tt]= t.join(": "); }
+			}
+			return nh;
+		}
+		return false;
+	},
+	Close: function (R){ if(this._RequestStack[R]){ this.Stop(R); this._RequestStack[R]= null; } },
+	Stop: function (R){	if(this._RequestStack[R]){ this._RequestStack[R].Core.abort(); } },
+	ResponseXML: function (R){ return (this._RequestStack[R]?this._RequestStack[R].Core.responseXML:false); },
+	ResponseText: function (R){ return (this._RequestStack[R]?this._RequestStack[R].Core.responseText:false); },
+	Status: function (R){ return (this._RequestStack[R]?this._RequestStack[R].Core.status:false); },
+	Headers: function (R,H,V){ return this.Header(R,H,V); },
+	SetHeader: function (R,H,V){ return this.Header(R,H,V); },
+	State: function (R){ return (this._RequestStack[R]?this._RequestStack[R].State:this.States.Closed); }
+};
+window.$Ajax= $Ajax;
