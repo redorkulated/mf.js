64589dcfdcbdcab32d18d71bd352971138bab398
Michael <michael@fallo.ws>
2013-08-30 18:02:59 +0100

    Version 2.1
    * Added some more error handling
    * Added method to read ErrorText for request
    * Bug fix on HEAD requests

--- mf.js
+++ mf.js
@@ -2,7 +2,7 @@
 * mf.js
 * @package com.mf89.js
 * @author Michael Fallows <michael@fallo.ws>
-* @version 2.0
+* @version 2.1
 */
 /* Basics */
 function $(i){ return document.getElementById(i); }
@@ -33,13 +33,13 @@ var $Ajax= {
 	Send: function(R,functionCallback,content){
 		if(this._RequestStack[R]){
 			if(!functionCallback){
-				this._RequestStack[R].Error="$Ajax.send callback function not found";
+				this._RequestStack[R].ErrorText="$Ajax.send callback function not found";
 				console.log("Error with $Ajax Request #"+ R + " : $Ajax.send callback function not found");
 				return false;
 			}
 			if(this._RequestStack[R].Method == "HEAD"){ //Head request, will return no content just headers/status
 				this._RequestStack[R].Core.onreadystatechange= function(){
-					if($Ajax.CoreRequest(R).readyState == 4){
+					if(this.readyState == 4){
 						this._Parent.State= this._Ajax.States.Received;
 						functionCallback(this.status,this._Parent.ID);
 					}
@@ -56,7 +56,7 @@ var $Ajax= {
 			}
 			this._RequestStack[R].State= this.States.Sent;
 			try{ this._RequestStack[R].Core.send(content); } catch(e){
-				this._RequestStack[R].Error= e;
+				this._RequestStack[R].ErrorText= e;
 				this._RequestStack[R].State= this.States.ReceivedError;
 				console.log("Error with $Ajax Request #"+ R + " : "+ e);
 				functionCallback(false,R);
@@ -65,14 +65,14 @@ var $Ajax= {
 	},
 	Header: function (R,H,V){
 		if(!this._RequestStack[R]){ return false; }
-		if(this._RequestStack[R].State != this.States.Received && this._RequestStack[R].State != this.States.Open){ return false; }
+		if(this._RequestStack[R].State != this.States.Received && this._RequestStack[R].State != this.States.Open){ this._RequestStack[R].ErrorText="Wrong State"; return false; }
 		if(H){
 			if(!V && this._RequestStack[R].State == this.States.Received){ return this._RequestStack[R].Core.getResponseHeader(H); }
 			else if(V && this._RequestStack[R].State == this.States.Open){
 				this._RequestStack[R].Core.setRequestHeader(H,V);
 				return true;
 			}
-			return false;
+			this._RequestStack[R].ErrorText="Wrong State"; return false;
 		}else if(this._RequestStack[R].State == this.States.Received){
 			var oh= this._RequestStack[R].Core.getAllResponseHeaders().split("\n");
 			var nh= []; var tt;
@@ -81,15 +81,16 @@ var $Ajax= {
 			}
 			return nh;
 		}
-		return false;
+		this._RequestStack[R].ErrorText="Wrong State"; return false;
 	},
 	Close: function (R){ if(this._RequestStack[R]){ this.Stop(R); this._RequestStack[R]= null; } },
 	Stop: function (R){	if(this._RequestStack[R]){ this._RequestStack[R].Core.abort(); } },
-	ResponseXML: function (R){ return (this._RequestStack[R]?this._RequestStack[R].Core.responseXML:false); },
-	ResponseText: function (R){ return (this._RequestStack[R]?this._RequestStack[R].Core.responseText:false); },
-	Status: function (R){ return (this._RequestStack[R]?this._RequestStack[R].Core.status:false); },
+	ResponseXML: function (R){ return (this._RequestStack[R] && (this._RequestStack[R].State == this.States.Received || this._RequestStack[R].State != this.States.ReceivedError)?this._RequestStack[R].Core.responseXML:false); },
+	ResponseText: function (R){ return (this._RequestStack[R] && (this._RequestStack[R].State == this.States.Received || this._RequestStack[R].State != this.States.ReceivedError)?this._RequestStack[R].Core.responseText:false); },
+	Status: function (R){ return (this._RequestStack[R] && (this._RequestStack[R].State == this.States.Received || this._RequestStack[R].State != this.States.ReceivedError)?this._RequestStack[R].Core.status:false); },
 	Headers: function (R,H,V){ return this.Header(R,H,V); },
 	SetHeader: function (R,H,V){ return this.Header(R,H,V); },
-	State: function (R){ return (this._RequestStack[R]?this._RequestStack[R].State:this.States.Closed); }
+	State: function (R){ return (this._RequestStack[R]?this._RequestStack[R].State:this.States.Closed); },
+	ErrorText: function (R){ return (this._RequestStack[R]?this._RequestStack[R].ErrorText:false); }
 };
 window.$Ajax= $Ajax;
